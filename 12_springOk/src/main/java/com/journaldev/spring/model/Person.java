09a89ea51b3a78b25187e7887c79386904976e6a
Curso mañana <Curso maana@PC08a5>
2018-02-07 14:58:25 +0100

    f

---
+++ 12_springOk/src/main/java/com/journaldev/spring/model/Person.java
@@ -0,0 +1,78 @@
+package com.journaldev.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity bean with JPA annotations
+ * Hibernate provides JPA implementation
+ * @author pankaj
+ *
+ */
+@Entity
+@Table(name="clientes")
+public class Person {
+
+	@Id
+	@Column(name="idCliente")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private int idCliente;
+	private String nombre;
+	private String ciudad;
+	private String telefono;
+	private String mail;
+	
+	
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	@Override
+	public String toString(){
+		return "idCliente="+idCliente+", nombre="+nombre+", ciudad="+ciudad+", telefono="+telefono+", mail="+mail;
+	}
+}
